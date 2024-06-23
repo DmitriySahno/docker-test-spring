a3987f8e1f48b2f885a6243b2f2f2ffb1b486e44
DmitriySahno <dim4one1988@gmail.com>
2024-06-23 13:48:38 +0300

    added cross-origins

--- src/main/java/org/sahd/dockertest/config/WebMVCConfig.java
+++ src/main/java/org/sahd/dockertest/config/WebMVCConfig.java
@@ -13,7 +13,8 @@ public class WebMVCConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**");
+                registry.addMapping("/**")
+                        .allowedOrigins("*");
             }
         };
     }
